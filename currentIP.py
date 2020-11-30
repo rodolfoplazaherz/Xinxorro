ca839688921cd11a006cccdc39f00b378b0722bf
Rodolfo <rodolfoplazaherz@gmail.com>
2020-11-30 20:54:00 +0100

    dev: subiendo archivos desde RpI

--- currentIP.py
+++ currentIP.py
@@ -3,22 +3,26 @@ import smtplib
 import time
 import os
 import subprocess
+import getpass
 
 time.sleep(10)
 
 email = "dplaza@tryento.com"
 password = "72e(q|{ih2yT"
-to = "diegoplazau@hotmail.com"
+to = ["rodolfoplaza@hotmail.com", "diegoplazau@hotmail.com"]
 
 hostname = socket.gethostname()
-local_ip = subprocess.check_output('hostname -I', shell=True) 
+username = getpass.getuser()
+local_ip = subprocess.check_output('hostname -I', shell=True)
+
+local_ip = local_ip.decode('ascii')
 
 with smtplib.SMTP_SSL('35.214.219.34', 465) as smtp:
 
     smtp.login(email, password)
 
     subject = "{} {}".format(hostname, time.ctime())
-    body = "{}".format(local_ip)
+    body = " El numero IP para conectarse al usuario <{}> es: \n {}".format(username, local_ip)
 
     msg = "Subject: {}\n\n{}".format(subject, body)
 
