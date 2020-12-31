d0acb44db83a2e1105c0e64775305357fe72c8f8
Diego Alejandro Plaza Urrutia <diego.plaza.urrutia@gmail.com>
2020-12-31 15:26:26 +0100

    dev: testing results and pigpiod

--- controller.py
+++ controller.py
@@ -2,6 +2,10 @@ from pigpio_dht import DHT22
 import RPi.GPIO as GPIO
 import time
 import os
+import pigpio
+
+# this connects to the pigpio daemon which must be started first
+pi = pigpio.pi()
 
 os.system("sudo pigpiod")
 
