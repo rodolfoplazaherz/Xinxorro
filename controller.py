4c510b1af09a6f14e081dc6ec301d56095f2d739
Diego Alejandro Plaza Urrutia <diego.plaza.urrutia@gmail.com>
2021-01-16 19:30:56 +0100

    dev: deleting schedule module

--- controller.py
+++ controller.py
@@ -2,9 +2,7 @@ import RPi.GPIO as GPIO
 import Adafruit_DHT
 import time
 import os
-import sched
 import csv
-
 
 MUSHROOM_TYPE = ""
 TENT_LENGHT_M = 0.6
@@ -46,58 +44,40 @@ try:
     initTime = time.time()
     gpioSetup()
     relayStatus = False
-    
-    s = sched.scheduler(time.time, time.sleep)
-    print("init scheduler")
-    s.enter(
-        AIR_EXCHANGE_PERIOD_MINUTES * 60,
-        AIR_EXCHANGE_DURATION_MINUTES * 60,
-        relayON, argument=(27,)
-    )
-    print("appending task")
-    s.enter(
-        AIR_EXCHANGE_DURATION_MINUTES * 60 +
-        (AIR_EXCHANGE_PERIOD_MINUTES * 60 - AIR_EXCHANGE_DURATION_MINUTES * 60),
-        AIR_EXCHANGE_PERIOD_MINUTES * 60 - AIR_EXCHANGE_DURATION_MINUTES * 60,
-        relayOFF, argument=(27,)
-    )
-    print("appending task")
-    s.run()
-    print("running task")
-    # with open('historicalData.csv', 'w', newline='') as file:
-    #     writer = csv.writer(file)
-    #     while True:
-    #         time.sleep(2)
-    #         sensor = Adafruit_DHT.DHT22
-    #         timeStamp = time.time()
-    #         resultHumidity, resultTemperature = Adafruit_DHT.read_retry(
-    #             sensor, 17)
-    #         resultHumidity = round(resultHumidity, 2)
-    #         resultTemperature = round(resultTemperature, 2)
-    #         writer.writerow([resultHumidity, resultTemperature, timeStamp])
-    #         if resultHumidity == None or resultTemperature == None:
-    #             # If sensor is not meausuring properly, then program sleeps for 2 sec
-    #             waitXSeconds(2)
-    #         else:
-    #             print("rh:{}, °C:{}, time:{}".format(
-    #                 resultHumidity, resultTemperature, timeStamp))
-    #             if resultHumidity < 80:
-    #                 if resultHumidity < 50 and relayStatus == True:
-    #                     waitXSeconds(0)
-    #                     print('Very low humidity or atomizer is broken')
-    #                 else:
-    #                     relayStatus = relayON(27)
-    #                     waitXSeconds(0)
-    #                     print('Low humidity')
-    #             elif resultHumidity > 85 and relayStatus == True:
-    #                 waitXSeconds(0)
-    #                 relayStatus = relayOFF(27)
-    #                 print('High humidity, turning OFF the relay')
-    #             elif 80 <= resultHumidity <= 85:
-    #                 relayStatus = relayOFF(27)
-    #                 print("Perfect Humidity, relayOFF")
-    #             else:
-    #                 pass
+    with open('historicalData.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        while True:
+            time.sleep(2)
+            sensor = Adafruit_DHT.DHT22
+            timeStamp = time.time()
+            resultHumidity, resultTemperature = Adafruit_DHT.read_retry(
+                sensor, 17)
+            resultHumidity = round(resultHumidity, 2)
+            resultTemperature = round(resultTemperature, 2)
+            writer.writerow([resultHumidity, resultTemperature, timeStamp])
+            if resultHumidity == None or resultTemperature == None:
+                # If sensor is not meausuring properly, then program sleeps for 2 sec
+                waitXSeconds(2)
+            else:
+                print("rh:{}, °C:{}, time:{}".format(
+                    resultHumidity, resultTemperature, timeStamp))
+                if resultHumidity < 80:
+                    if resultHumidity < 50 and relayStatus == True:
+                        waitXSeconds(0)
+                        print('Very low humidity or atomizer is broken')
+                    else:
+                        relayStatus = relayON(27)
+                        waitXSeconds(0)
+                        print('Low humidity')
+                elif resultHumidity > 85 and relayStatus == True:
+                    waitXSeconds(0)
+                    relayStatus = relayOFF(27)
+                    print('High humidity, turning OFF the relay')
+                elif 80 <= resultHumidity <= 85:
+                    relayStatus = relayOFF(27)
+                    print("Perfect Humidity, relayOFF")
+                else:
+                    pass
 except KeyboardInterrupt:
     print('cancelled by the user, cleaning')
 finally:
