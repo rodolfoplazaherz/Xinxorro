a3150641092f111da219d988721c9d1c132ff8bc
Diego Alejandro Plaza Urrutia <diego.plaza.urrutia@gmail.com>
2021-01-16 13:44:20 +0100

    dev: creating a csv file to push the data

--- controller.py
+++ controller.py
@@ -28,34 +28,34 @@ try:
     import csv
     with open('historicalData.csv', 'w', newline='') as file:
         writer = csv.writer(file)
-    while True:
-        time.sleep(2)
-        sensor = Adafruit_DHT.DHT22
-        timeStamp = time.ctime()
-        resultHumidity, resultTemperature = Adafruit_DHT.read_retry(sensor, 17)
-        resultHumidity = round(resultHumidity, 2)
-        resultTemperature = round(resultTemperature, 2)
-        writer.writerow([resultHumidity, resultTemperature, timeStamp])
-        if resultHumidity == None or resultTemperature == None:
-            waitXSeconds(2) #If sensor is not meausuring properly, then program sleeps for 2 sec
-        else:
-            print("rh:{}, °C:{}, time:{}".format(resultHumidity, resultTemperature, timeStamp))
-            if resultHumidity < 80:
-                if resultHumidity < 50 and relayStatus == True:
-                    waitXSeconds(0)
-                    print('Very low humidity or atomizer is broken')
-                else:
-                    relayStatus = relayON(27)
-                    waitXSeconds(0)
-                    print('Low humidity')
-            elif resultHumidity > 85 and relayStatus == True:
-                waitXSeconds(0)
-                relayStatus = relayOFF(27)
-                print('High humidity')
-            elif 80 <= resultHumidity <= 85:
-                print("Perfect Humidity")
+        while True:
+            time.sleep(2)
+            sensor = Adafruit_DHT.DHT22
+            timeStamp = time.ctime()
+            resultHumidity, resultTemperature = Adafruit_DHT.read_retry(sensor, 17)
+            resultHumidity = round(resultHumidity, 2)
+            resultTemperature = round(resultTemperature, 2)
+            writer.writerow([resultHumidity, resultTemperature, timeStamp])
+            if resultHumidity == None or resultTemperature == None:
+                waitXSeconds(2) #If sensor is not meausuring properly, then program sleeps for 2 sec
             else:
-                pass
+                print("rh:{}, °C:{}, time:{}".format(resultHumidity, resultTemperature, timeStamp))
+                if resultHumidity < 80:
+                    if resultHumidity < 50 and relayStatus == True:
+                        waitXSeconds(0)
+                        print('Very low humidity or atomizer is broken')
+                    else:
+                        relayStatus = relayON(27)
+                        waitXSeconds(0)
+                        print('Low humidity')
+                elif resultHumidity > 85 and relayStatus == True:
+                    waitXSeconds(0)
+                    relayStatus = relayOFF(27)
+                    print('High humidity')
+                elif 80 <= resultHumidity <= 85:
+                    print("Perfect Humidity")
+                else:
+                    pass
 except KeyboardInterrupt:
     print('cancelled by the user')
 finally:
