df747ac4323d56396ae59fa3dcf6c1e8c17c0506
Diego Alejandro Plaza Urrutia <diego.plaza.urrutia@gmail.com>
2021-01-16 15:21:35 +0100

    dev: optimizing code

--- controller.py
+++ controller.py
@@ -1,4 +1,3 @@
-
 import RPi.GPIO as GPIO
 import Adafruit_DHT
 import time
@@ -6,21 +5,26 @@ import os
 
 relayStatus = False
 
+
 def gpioSetup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(27, GPIO.OUT)
-    GPIO.output(27,GPIO.LOW)
+    GPIO.output(27, GPIO.LOW)
+
 
 def relayON(gpioNumber):
     GPIO.output(gpioNumber, GPIO.HIGH)
     return True
 
+
 def relayOFF(gpioNumber):
     GPIO.output(gpioNumber, GPIO.LOW)
     return False
 
+
 def waitXSeconds(n):
     time.sleep(n)
+
 
 try:
     gpioSetup()
@@ -32,14 +36,17 @@ try:
             time.sleep(2)
             sensor = Adafruit_DHT.DHT22
             timeStamp = time.ctime()
-            resultHumidity, resultTemperature = Adafruit_DHT.read_retry(sensor, 17)
+            resultHumidity, resultTemperature = Adafruit_DHT.read_retry(
+                sensor, 17)
             resultHumidity = round(resultHumidity, 2)
             resultTemperature = round(resultTemperature, 2)
             writer.writerow([resultHumidity, resultTemperature, timeStamp])
             if resultHumidity == None or resultTemperature == None:
-                waitXSeconds(2) #If sensor is not meausuring properly, then program sleeps for 2 sec
+                # If sensor is not meausuring properly, then program sleeps for 2 sec
+                waitXSeconds(2)
             else:
-                print("rh:{}, °C:{}, time:{}".format(resultHumidity, resultTemperature, timeStamp))
+                print("rh:{}, °C:{}, time:{}".format(
+                    resultHumidity, resultTemperature, timeStamp))
                 if resultHumidity < 80:
                     if resultHumidity < 50 and relayStatus == True:
                         waitXSeconds(0)
@@ -52,11 +59,9 @@ try:
                     waitXSeconds(0)
                     relayStatus = relayOFF(27)
                     print('High humidity, turning OFF the relay')
-                elif 80 <= resultHumidity <= 85 and relayStatus == True:
+                elif 80 <= resultHumidity <= 85:
                     relayStatus = relayOFF(27)
                     print("Perfect Humidity, relayOFF")
-                elif 80 <= resultHumidity <= 85 and relayStatus == False:
-                    print("Perfect Humidity, keep environment as it is")
                 else:
                     pass
 except KeyboardInterrupt:
