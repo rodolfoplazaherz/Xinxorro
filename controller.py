cd38bd4b894746d350d2333a2d9fab43478d24b2
Diego Alejandro Plaza Urrutia <diego.plaza.urrutia@gmail.com>
2021-01-16 20:08:22 +0100

    dev: adding ventilator scheduler

--- controller.py
+++ controller.py
@@ -15,6 +15,10 @@ AIR_EXCHANGES_PER_HOUR = 9
 AIR_EXCHANGE_PERIOD_MINUTES = 60/AIR_EXCHANGES_PER_HOUR
 AIR_EXCHANGE_DURATION_MINUTES = TENT_VOLUME_M3/FAN_CAPACITY_M3_PER_MINUTE
 
+HUMIDIFIER_GPIO = 0
+VENTILATOR_GPIO = 27
+SENSOR_DHT22_GPIO = 17
+
 
 relayStatus = False
 
@@ -38,18 +42,18 @@ def waitXSeconds(n):
     time.sleep(n)
 
 
-try:
-    initTime = time.time()
-    gpioSetup()
-    relayStatus = False
+def ventilatorController():
+    pass
+
+
+def sensorController(relayStatus):
     with open('historicalData.csv', 'w', newline='') as file:
         writer = csv.writer(file)
         while True:
-            time.sleep(2)
             sensor = Adafruit_DHT.DHT22
             timeStamp = time.time()
             resultHumidity, resultTemperature = Adafruit_DHT.read_retry(
-                sensor, 17)
+                sensor, SENSOR_DHT22_GPIO)
             resultHumidity = round(resultHumidity, 2)
             resultTemperature = round(resultTemperature, 2)
             if resultHumidity == None or resultTemperature == None:
@@ -63,18 +67,29 @@ try:
                         waitXSeconds(0)
                         print('Very low humidity or atomizer is broken')
                     else:
-                        relayStatus = relayON(27)
+                        relayStatus = relayON(HUMIDIFIER_GPIO)
                         waitXSeconds(0)
                         print('Low humidity')
                 elif resultHumidity > 80:
                     waitXSeconds(0)
-                    relayStatus = relayOFF(27)
+                    relayStatus = relayOFF(HUMIDIFIER_GPIO)
                     print('High humidity, turning OFF the relay')
                 else:
                     pass
-            writer.writerow([resultHumidity, resultTemperature, timeStamp, relayStatus])
+            writer.writerow(
+                [resultHumidity, resultTemperature, timeStamp, relayStatus])
 
-except KeyboardInterrupt:
-    print("Cancelled by the user, cleaning")
-finally:
-    GPIO.cleanup()
+
+def main():
+    try:
+        gpioSetup()
+        relayStatus = False
+        sensorController(relayStatus)
+    except KeyboardInterrupt:
+        print("Cancelled by the user, cleaning")
+    finally:
+        GPIO.cleanup()
+
+
+if __name__ == "__main__":
+    main()
