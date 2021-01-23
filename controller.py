dd7e0df55b0884d761348554ae2a6f684e35c21a
dplaza <dplaza@gmx.com>
2021-01-23 13:57:19 +0100

    dev: changing controller.py

--- controller.py
+++ controller.py
@@ -1,35 +1,29 @@
-import RPi.GPIO as GPIO
-import Adafruit_DHT
-import time
-import datetime
 import os
 import csv
-import sched
+import time
+import datetime
+import Adafruit_DHT
+import RPi.GPIO as GPIO
+from ConfigurationHandler import loadConfigData
 
-MUSHROOM_TYPE = ""
-TENT_LENGHT_M = 0.6
-TENT_WIDTH_M = 0.6
-TENT_HEIGHT_M = 1.6
+Config = loadConfigData()
+
+MUSHROOM_TYPE = Config("MUSHROOM_TYPE")
+TENT_LENGHT_M = Config("TENT_LENGHT_M")
+TENT_WIDTH_M = Config("TENT_WIDTH_M")
+TENT_HEIGHT_M = Config("TENT_HEIGHT_M")
+FAN_CAPACITY_M3_PER_HOUR = Config("FAN_CAPACITY_M3_PER_HOUR")
+AIR_EXCHANGES_PER_HOUR = Config("AIR_EXCHANGES_PER_HOUR")
+
 TENT_VOLUME_M3 = TENT_LENGHT_M * TENT_WIDTH_M * TENT_HEIGHT_M
-FAN_CAPACITY_M3_PER_HOUR = 100
 FAN_CAPACITY_M3_PER_MINUTE = FAN_CAPACITY_M3_PER_HOUR/60
-AIR_EXCHANGES_PER_HOUR = 9
 AIR_EXCHANGE_PERIOD_MINUTES = 60/AIR_EXCHANGES_PER_HOUR
 AIR_EXCHANGE_DURATION_MINUTES = TENT_VOLUME_M3/FAN_CAPACITY_M3_PER_MINUTE
 
 
-HUMIDIFIER_GPIO = 0
-VENTILATOR_GPIO = 27
-SENSOR_DHT22_GPIO = 17
-
-
-relayStatus = False
-s = sched.scheduler(time.time, time.sleep)
-
-
 def gpioSetup():
     GPIO.setmode(GPIO.BCM)
-    for gpio in [HUMIDIFIER_GPIO, VENTILATOR_GPIO, SENSOR_DHT22_GPIO]:
+    for gpio in [Config("HUMIDIFIER_GPIO"), Config("VENTILATOR_GPIO"), Config("SENSOR_DHT22_GPIO")]:
         GPIO.setup(gpio, GPIO.OUT)
     return True
 
@@ -50,10 +44,10 @@ def ventilatorController(relayStatus):
     i = 0
     while True:
         print("TURNING ON")
-        relayON(VENTILATOR_GPIO)
+        relayON(Config("VENTILATOR_GPIO"))
         time.sleep(2)#(AIR_EXCHANGE_DURATION_MINUTES * 60)
         print("TURNING OFF")
-        relayOFF(VENTILATOR_GPIO)
+        relayOFF(Config("VENTILATOR_GPIO"))
         time.sleep(5)#((AIR_EXCHANGE_PERIOD_MINUTES -
                   #  AIR_EXCHANGE_DURATION_MINUTES) * 60)
         i += 1
@@ -75,7 +69,7 @@ def sensorController(relayStatus):
             sensor = Adafruit_DHT.DHT22
             timeStamp = datetime.datetime.now()
             resultHumidity, resultTemperature = Adafruit_DHT.read_retry(
-                sensor, SENSOR_DHT22_GPIO)
+                sensor, Config("SENSOR_DHT22_GPIO"))
             resultHumidity = round(resultHumidity, 2)
             resultTemperature = round(resultTemperature, 2)
             if resultHumidity == None or resultTemperature == None:
@@ -88,9 +82,9 @@ def sensorController(relayStatus):
                     if relayStatus == True:
                         print("Check integrity of the environment")
                     else:
-                        relayStatus = relayON(HUMIDIFIER_GPIO)
+                        relayStatus = relayON(Config("HUMIDIFIER_GPIO"))
                 elif resultHumidity > 80:
-                    relayStatus = relayOFF(HUMIDIFIER_GPIO)
+                    relayStatus = relayOFF(Config("HUMIDIFIER_GPIO"))
                 else:
                     pass
             writer.writerow(
