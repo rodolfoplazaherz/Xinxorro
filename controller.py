d4e3b0bbc8cd17a6781a2229954c94888eb8a5e2
dplaza <dplaza@gmx.com>
2021-01-23 19:20:30 +0100

    dev: installing everything

--- controller.py
+++ controller.py
@@ -24,14 +24,14 @@ def gpioSetup():
 
 def relayON(gpioNumber):
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(gpio, GPIO.OUT)
+    GPIO.setup(gpioNumber, GPIO.OUT)
     GPIO.output(gpioNumber, GPIO.LOW)
     return True
 
 
 def relayOFF(gpioNumber):
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(gpio, GPIO.OUT)
+    GPIO.setup(gpioNumber, GPIO.OUT)
     GPIO.output(gpioNumber, GPIO.HIGH)
     return False
 
